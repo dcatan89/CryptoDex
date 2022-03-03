a10c38dd57ef2246434e6dffb0797e33027c1af6
Daniel Catan <danieljcatan@gmail.com>
2022-03-03 11:42:14 -0800

    click event for icon and search bar completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 /* Hamburger Icon click */
 var $hamburgerIcon = document.querySelector('.hamburger-icon');
 var $dropMenu = document.querySelector('.choice-list');
+var $menuSearch = document.querySelector('.search-header');
+var $searchBar = document.querySelector('.search-nav-row');
+var menu = 'closed';
+var bar = 'closed';
+
 function hamburgerClick(event) {
-  $dropMenu.classList.remove('hidden');
-  $dropMenu.classList.add('open');
+  if (menu === 'closed') {
+    $dropMenu.classList.remove('hidden');
+    menu = 'open';
+  } else {
+    $dropMenu.classList.add('hidden');
+    $searchBar.classList.add('hidden');
+    menu = 'closed';
+  }
+}
+
+function handleSearchBar(event) {
+  event.preventDefault();
+  if (bar === 'closed') {
+    $searchBar.classList.remove('hidden');
+    bar = 'open';
+  } else {
+    $searchBar.classList.add('hidden');
+    bar = 'closed';
+  }
 }
 
 $hamburgerIcon.addEventListener('click', hamburgerClick);
+$menuSearch.addEventListener('click', handleSearchBar);
