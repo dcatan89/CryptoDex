71a49d15922964823a3cf84eb795e94380f5e805
dcatan89 <danieljcatan@gmail.com>
2022-03-03 20:14:09 +0000

    api listed

--- js/data.js
+++ js/data.js
@@ -5,3 +5,14 @@ var data = {
   nextId: 1,
   watchlist: []
 };
+
+var targetUrl = encodeURIComponent('https://api.coinpaprika.com/v1/coins');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+xhr.setRequestHeader('token', 'abc123');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+
+});
+xhr.send();
