7259298d472b474d1fabe4b030d6b34b1acf00ef
dcatan89 <danieljcatan@gmail.com>
2022-03-05 00:24:49 +0000

    Both search bars work and siplay data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 /* Hamburger Icon click */
 var $hamburgerIcon = document.querySelector('.hamburger-icon');
 var $dropMenu = document.querySelector('.choice-list');
@@ -21,6 +22,7 @@ function handleSearchBar(event) {
   event.preventDefault();
   if (bar === 'closed') {
     $searchBar.classList.remove('hidden');
+    $dropMenu.classList.add('hidden');
     bar = 'open';
   } else {
     $searchBar.classList.add('hidden');
@@ -53,6 +55,8 @@ xhr.addEventListener('load', function () {
 xhr.send();
 
 /* API Search Function and Data */
+var $formSearch = document.querySelector('#form-search');
+var $formSearch2 = document.querySelector('#form-search2');
 var $rank = document.querySelector('.for-rank');
 var $Symbol = document.querySelector('.for-symbol');
 var $CurrentPrice = document.querySelector('.for-price');
@@ -61,27 +65,46 @@ var $volume = document.querySelector('.for-volume');
 var $subHeader = document.querySelector('.sub-header-crypto');
 var $mainHeader = document.querySelector('.main-header');
 
-function handleId(id) {
+function searchID(id) {
   var value = id.toUpperCase();
   var xhr2 = new XMLHttpRequest();
   xhr2.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + apiUrl + value);
   xhr2.setRequestHeader('token', 'abc123');
   xhr2.responseType = 'json';
   xhr2.addEventListener('load', function () {
+
     var crypto = xhr2.response.asset;
 
     $subHeader.textContent = crypto.name;
-    $mainHeader.textContent = 'CryptoCurrency Assets';
-    $rank.textContent = Math.round(crypto.change_24h) + '%';
+    $mainHeader.textContent = 'Assets';
+    $rank.textContent = Math.round(crypto.change_24h * 100) / 100 + '%';
     $Symbol.textContent = crypto.asset_id;
-    $CurrentPrice.textContent = '$ ' + Math.round(crypto.price);
+    $CurrentPrice.textContent = '$ ' + Math.round(crypto.price * 100) / 100;
     $Cap.textContent = '$ ' + Math.round(crypto.market_cap);
     $volume.textContent = '$ ' + Math.round(crypto.volume_24h);
   });
   xhr2.send();
 }
 
-handleId('Eth');
+function handleSubmit1(event) {
+  event.preventDefault();
+  var searchBar1Value = $formSearch.elements.searchbar.value;
+  searchID(searchBar1Value);
+  dataView('cryptos');
+  $formSearch.reset();
+}
+
+function handleSubmit2(event) {
+  event.preventDefault();
+  var searchBar2Value = $formSearch2.elements.searchbar2.value;
+  searchID(searchBar2Value);
+  $dropMenu.classList.add('hidden');
+  dataView('cryptos');
+  $formSearch2.reset();
+}
+
+$formSearch.addEventListener('submit', handleSubmit1);
+$formSearch2.addEventListener('submit', handleSubmit2);
 /* Data View function */
 var $view = document.querySelectorAll('.view');
 
@@ -95,4 +118,4 @@ function dataView(string) {
     }
   }
 }
-dataView('cryptos');
+dataView('global');
