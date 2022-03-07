d276f47ed4b190953d973cea879fb2609a2220d3
dcatan89 <danieljcatan@gmail.com>
2022-03-07 23:46:31 +0000

    local storage working and saving entries

--- js/data.js
+++ js/data.js
@@ -5,3 +5,15 @@ var data = {
   nextId: 1,
   watchlist: []
 };
+
+var previousDataJSON = localStorage.getItem('data-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function beforeUnloadListener(event) {
+  var $data = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', $data);
+}
+
+window.addEventListener('beforeunload', beforeUnloadListener);
